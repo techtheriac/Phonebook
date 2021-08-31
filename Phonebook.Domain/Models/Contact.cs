eee5fbee6b67d55ff6a3e00d13a998bae1a13348
techtheriac <jezreel.franklin@decagon.dev>
2021-08-31 22:35:21 +0100

    Add project files.

---
+++ Phonebook.Domain/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Domain.Models
+{
+    public class Contact
+    {
+        [Key]
+        public string Id {  get; set; } 
+        public string FirstName {  get; set; }
+        public string LastName {  get; set; }
+        public string Email {  get; set; }  
+        public string Phone {  get; set; }
+    }
+}
